681565fb9528bade35826b299b7e6ba9005af048
Yura_Groshkov <unfreakk@mail.ru>
2023-10-26 19:28:39 +0300

    делал регистрацию ну и да

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css'
 import './companents/button.css'
 import serverLogo from './assets/img/server-logo.svg'
-
+import passwordShow from './assets/img/password-show-icon.svg'
+import maleIcon from './assets/img/maleIcon.svg'
+import femaleIcon from './assets/img/femaleIcon.svg'
 
 const pageWrapper = document.createElement('div')
 pageWrapper.classList.add('page-wrapper')
@@ -9,98 +11,195 @@ document.body.append(pageWrapper)
 
 // авторизация
 
-
 const logo = document.createElement('img')
 logo.src = serverLogo
 logo.alt = 'server-logo'
 logo.classList.add('logo')
 pageWrapper.append(logo)
 
-const authorizationTitle = document.createElement('span')
-authorizationTitle.classList.add('authorization-title')
-authorizationTitle.textContent = 'Авторизация'
-pageWrapper.append(authorizationTitle)
+// ОКНО АВТОРИЗАЦИИ!!!!
 
-const inputLogin = document.createElement('input');
-inputLogin.classList.add('input-login', 'input')
-inputLogin.type = 'text';
-inputLogin.placeholder = 'Введите логин'
-pageWrapper.append(inputLogin)
+const authTitle = document.createElement('div')
+authTitle.classList.add('authorization-title')
+authTitle.textContent = 'Авторизация'
+pageWrapper.append(authTitle)
 
-const inputPassword = document.createElement('input');
-inputPassword.classList.add('input-password', 'input')
-inputPassword.type = 'password'
-inputPassword.placeholder = 'Введите пароль'
-pageWrapper.append(inputPassword)
+const authLoginInput = document.createElement('input');
+authLoginInput.classList.add('input-login', 'input')
+authLoginInput.type = 'text';
+authLoginInput.placeholder = 'Введите логин'
+pageWrapper.append(authLoginInput)
 
+const authPasswordInput = document.createElement('input');
+authPasswordInput.classList.add('input-password', 'input')
+authPasswordInput.id = 'input-password'
+authPasswordInput.type = 'password'
+authPasswordInput.placeholder = 'Введите пароль'
+
+// иконка глазика
+
+function createPasswordShowIcon() {
+  const passwordShowIcon = document.createElement('img')
+  passwordShowIcon.src = passwordShow
+  passwordShowIcon.alt = 'password-show-icon'
+  passwordShowIcon.classList.add('password-control')
+  return passwordShowIcon
+}
+
+let authPasswordShow = createPasswordShowIcon()
+
+// после с паролем при авторизации
+const authPasswordArea = document.createElement('div')
+authPasswordArea.classList.add('authorization-password-area')
+authPasswordArea.append(authPasswordInput, authPasswordShow)
+pageWrapper.append(authPasswordArea)
+
+
+// Запомнить меня
 const passwordCheckboxArea = document.createElement('div')
 passwordCheckboxArea.classList.add('form-group')
+
 const passwordCheckbox = document.createElement('input')
 passwordCheckbox.classList.add('my-checkbox')
 passwordCheckbox.type = 'checkbox'
 passwordCheckbox.id = 'checkbox'
+
 const passwordCheckboxText = document.createElement('label')
 passwordCheckboxText.htmlFor = 'checkbox'
 passwordCheckboxText.textContent = 'Запомнить меня'
 
-passwordCheckboxArea.append(passwordCheckbox)
-passwordCheckboxArea.append(passwordCheckboxText)
-
-
+passwordCheckboxArea.append(passwordCheckbox, passwordCheckboxText)
 pageWrapper.append(passwordCheckboxArea)
 
-// уведомления в нижней части экрана
 
-const MainNotification = document.createElement('div')
-MainNotification.classList.add('main-notification', 'hidden')
-document.body.append(MainNotification)
+// Кнопки для авторизации
 
-function clearMainNotification() {
-  MainNotification.classList.add('hidden')
-}
-
-
-// авторизация и регистрация
-// const authorizationArea = document.createElement('div')
-// authorizationArea.classList.add('authorization-area')
-// pageWrapper.appendChild(authorizationArea)
+const authButton = document.createElement('button')
+authButton.textContent = 'Войти'
+authButton.classList.add('login-button', 'primary-button', 'btn')
 
 const registrationButton = document.createElement('button')
 registrationButton.textContent = 'Регистрация'
 registrationButton.classList.add('registration-button', 'btn', 'secondary-button')
 
-const authorizationButton = document.createElement('button')
-authorizationButton.textContent = 'Войти'
-authorizationButton.classList.add('login-button', 'primary-button', 'btn')
+pageWrapper.append(authButton, registrationButton)
 
-pageWrapper.append(authorizationButton)
-pageWrapper.append(registrationButton)
+// функция показать/скрыть пароль
+
+authPasswordShow.addEventListener('click', (e) => {
+  onClickPasswordShow(authPasswordInput)
+})
+
+function onClickPasswordShow(input) {
+  if (input.type === 'password') {
+    input.type = 'text'
+  } else {
+    input.type = 'password'
+  }
+  return false;
+}
 
 
 
 
-const registrationInput = document.createElement('input');
-registrationInput.classList.add('input-login', 'input')
-registrationInput.type = 'text';
-registrationInput.placeholder = 'Придумайте ваш логин';
+// ОКНО РЕГИСТРАЦИИ !!!!!!!!!!!!!!!!!!!!!!!!!
 
-const registrationText = document.createElement('div')
-registrationText.textContent = 'Регистрация'
+const registrationTitle = document.createElement('span')
+registrationTitle.textContent = 'РЕГИСТРАЦИЯ'
 
-const registrationPassword = document.createElement('input');
-registrationPassword.classList.add('input-password', 'input')
-registrationPassword.type = 'password'
-registrationPassword.placeholder = 'Придумайте ваш пароль'
+const registrationInputLogin = document.createElement('input')
+registrationInputLogin.placeholder = 'Введите логин'
+registrationInputLogin.classList.add('input', 'input-login')
 
-// кнопка проверки на регистрацию
-const registrationCheckButton = document.createElement('button')
-registrationCheckButton.classList.add('registration-check-button')
-registrationCheckButton.textContent = 'Зарегистрироваться'
+// Введите пароль
+const registrationPasswordArea = document.createElement('div')
+registrationPasswordArea.id = 'input-password'
+registrationPasswordArea.classList.add('authorization-password-area')
 
-// кнопка отмены регистрации
-const declineButton = document.createElement('button')
-declineButton.classList.add('decline-button','hidden')
-declineButton.textContent = 'Отмена'
+const registrationPasswordInput = document.createElement('input');
+registrationPasswordInput.placeholder = 'Введите пароль'
+registrationPasswordInput.classList.add('input-password', 'input')
+
+let regPassIcon = createPasswordShowIcon()
+registrationPasswordArea.append(registrationPasswordInput, regPassIcon)
+
+// повторите пароль
+const confirmRegistrationPasswordArea = document.createElement('div')
+confirmRegistrationPasswordArea.id = 'input-password'
+confirmRegistrationPasswordArea.classList.add('authorization-password-area')
+
+regPassIcon.addEventListener('click', (e) => {
+  onClickPasswordShow(registrationPasswordInput)
+})
+
+const confirmRegistrationPasswordInput = document.createElement('input');
+confirmRegistrationPasswordInput.placeholder = 'Повторите пароль'
+confirmRegistrationPasswordInput.classList.add('input-password', 'input')
+
+
+let regPassConfirmIcon = createPasswordShowIcon()
+confirmRegistrationPasswordArea.append(confirmRegistrationPasswordInput,regPassConfirmIcon )
+
+regPassConfirmIcon.addEventListener('click', (e) => {
+  onClickPasswordShow(confirmRegistrationPasswordInput)
+})
+
+// электронная почта
+const regMailInput = document.createElement('input')
+regMailInput.placeholder = 'Электронная почта'
+regMailInput.classList.add('input')
+
+// кнопки смены пола
+const regGenderButtons = document.createElement('div')
+regGenderButtons.classList.add('reg-gender-buttons')
+
+const maleGenderButtonArea = document.createElement('div')
+maleGenderButtonArea.classList.add('gender-button-area')
+const maleGenderButton = document.createElement('button')
+maleGenderButton.classList.add('gender-button')
+const maleGenderIcon = document.createElement('img')
+maleGenderIcon.src = maleIcon
+maleGenderIcon.alt = 'male-icon'
+
+maleGenderButtonArea.append(maleGenderButton, maleGenderIcon)
+
+const femaleGenderButtonArea = document.createElement('div')
+femaleGenderButtonArea.classList.add('gender-button-area')
+const femaleGenderButton = document.createElement('button')
+femaleGenderButton.classList.add('gender-button')
+const femaleGenderIcon = document.createElement('img')
+femaleGenderIcon.src = femaleIcon
+femaleGenderIcon.alt = 'female-icon'
+
+femaleGenderButtonArea.append(femaleGenderButton, femaleGenderIcon)
+
+
+regGenderButtons.append(maleGenderButtonArea, femaleGenderButtonArea)
+
+
+
+registrationButton.addEventListener('click', () => {
+authTitle.classList.add('hidden')
+  authLoginInput.classList.add('hidden')
+  authPasswordArea.classList.add('hidden')
+  passwordCheckboxArea.classList.add('hidden')
+  registrationButton.classList.add('hidden')
+  authButton.classList.add('hidden')
+
+  pageWrapper.append(registrationTitle)
+  pageWrapper.append(registrationInputLogin)
+  pageWrapper.append(registrationPasswordArea)
+  pageWrapper.append(confirmRegistrationPasswordArea)
+  pageWrapper.append(regMailInput)
+  pageWrapper.append(regGenderButtons)
+})
+
+
+
+
+
+
+
 
 // регулярка для проверки логина
 const loginRegex = /[0-9a-zA-Z$^\-_]{4,24}/
