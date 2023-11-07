24e3ce0e29b849e44fb6eae0fb82bd0a4eb5dbac
danilov.kirill9 <danilov.kirill9@wb.ru>
2023-11-07 19:24:47 +0300

    Убираем фон в случае продакшена

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [new HtmlWebpackPlugin(
-        {
-            title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/template.html'), // шаблон
-            filename: 'index.html', // название выходного файла
-        }
-    )],
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          // { test: /\.svg$/, use: 'file-loader' },
+module.exports = function (env, argv) {
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        plugins: [new HtmlWebpackPlugin(
             {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
-                type: 'asset/inline',
-            },
-        ],
-      },
-      devServer: {
-        port: 3000
-      }
+                title: 'webpack Boilerplate',
+                template: path.resolve(__dirname, './src/template.html'), // шаблон
+                filename: 'index.html', // название выходного файла
+                templateParameters: {
+                    production: argv.mode == "production",
+                    development: argv.mode == "development",
+                },
+            }
+        )],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                // { test: /\.svg$/, use: 'file-loader' },
+                {
+                    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
+                    type: 'asset/inline',
+                },
+            ],
+        },
+        devServer: {
+            port: 3000
+        },
+    }
 }
